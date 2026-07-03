56f4b6d6981785bdeaee78c618843ac2544dca9d
ramziddinbahtiyorov <nilufarmirzarahimova543@gmail.com>
2026-07-03 13:30:53 +0300

    Add: 8) Substring bor-yo‘qligini tekshirish [ramziddinbahtiyorov]

---
+++ lms_kod_topshiriqlari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/08-8__Substring_bor-yo_qligini_tekshirish.py
@@ -0,0 +1,12 @@
+matn = input()
+substring = input()
+
+if substring in matn:
+    print("YES")
+else:
+    print("NO")
+
+
+# 1-qator: matn
+# 2-qator: qidiriladigan substring
+# Agar substring matnda bo'lsa "YES", aks holda "NO" chiqaring.
