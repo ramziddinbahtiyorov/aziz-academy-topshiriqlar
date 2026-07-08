8f5df2a0f1f8c1123cbfcb5eb74a8763ca91be5c
ramziddinbahtiyorov <nilufarmirzarahimova543@gmail.com>
2026-07-08 13:38:37 +0300

    Add: 11) Ikkita shart: yosh va ball [ramziddinbahtiyorov]

---
+++ lms_kod_topshiriqlari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/11-11__Ikkita_shart__yosh_va_ball.py
@@ -0,0 +1,10 @@
+yosh = int(input())
+ball = int(input())
+
+print(yosh <= 25 and ball >= 90)
+    
+    
+    #1-qator: yosh
+# 2-qator: ball
+# Talaba grant uchun mos bo'lsin: yosh <= 25 va ball >= 90
+# Shart natijasini (True/False) chiqaring.
