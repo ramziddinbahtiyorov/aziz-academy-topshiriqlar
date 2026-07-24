e03c2be150ca04fbe64e724fd8513a9664a5675a
ramziddinbahtiyorov <nilufarmirzarahimova543@gmail.com>
2026-07-24 13:38:11 +0300

    Add: 1) Ikki butun son: +, -, * [ramziddinbahtiyorov]

---
+++ lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/01-1__Ikki_butun_son_____-___.py
@@ -0,0 +1,12 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+print("Add:", a + b)
+print("Sub:", a - b)
+print("Mul:", a * b)
+
+
+# a va b (bitta qatorda)
+# 1-qator: "Add: <a+b>"
+# 2-qator: "Sub: <a-b>"
+# 3-qator: "Mul: <a*b>"
