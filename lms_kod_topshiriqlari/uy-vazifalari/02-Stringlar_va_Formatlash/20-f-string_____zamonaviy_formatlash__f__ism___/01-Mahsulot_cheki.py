a8ae3d9e4ac9632f388fa1be4305bef992d52a3c
ramziddinbahtiyorov <nilufarmirzarahimova543@gmail.com>
2026-09-04 13:40:30 +0300

    Add: Mahsulot cheki [ramziddinbahtiyorov]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,9 @@
+nom = input()
+narx = int(input())
+soni = int(input())
+jami = narx * soni
+
+print(f"Mahsulot: {nom}")
+print(f"Narxi: {narx} som")
+print(f"Soni: {soni}")
+print(f"Jami: {jami} som")
