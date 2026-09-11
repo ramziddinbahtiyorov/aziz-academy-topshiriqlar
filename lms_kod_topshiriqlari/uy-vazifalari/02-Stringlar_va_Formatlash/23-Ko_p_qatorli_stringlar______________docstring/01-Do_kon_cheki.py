cade96ae239d7969a9cd0da86f902661cf79220c
ramziddinbahtiyorov <nilufarmirzarahimova543@gmail.com>
2026-09-11 14:03:39 +0300

    Add: Do'kon cheki [ramziddinbahtiyorov]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/23-Ko_p_qatorli_stringlar______________docstring/01-Do_kon_cheki.py
@@ -0,0 +1,12 @@
+mahsulot = input()
+narx = int(input())
+soni = int(input())
+
+jami = narx * soni
+
+print(f"""===== CHEK =====
+Mahsulot: {mahsulot}
+Narx: {narx}
+Soni: {soni}
+Jami: {jami}
+================""")
