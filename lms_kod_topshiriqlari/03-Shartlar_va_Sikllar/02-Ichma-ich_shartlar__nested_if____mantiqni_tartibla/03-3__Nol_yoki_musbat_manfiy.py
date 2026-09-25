fda1042a173eb26553ff9e21b42696b6c27e69a5
ramziddinbahtiyorov <nilufarmirzarahimova543@gmail.com>
2026-09-25 13:20:19 +0300

    Add: 3) Nol yoki musbat/manfiy [ramziddinbahtiyorov]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/03-3__Nol_yoki_musbat_manfiy.py
@@ -0,0 +1,14 @@
+# a
+# Agar a == 0 bo'lsa "Zero"
+# Aks holda:
+#   agar a > 0 bo'lsa "Positive"
+#   aks holda "Negative"
+a = int(input())
+
+if a == 0:
+    print("Zero")
+else:
+    if a > 0:
+        print("Positive")
+    else:
+        print("Negative")
